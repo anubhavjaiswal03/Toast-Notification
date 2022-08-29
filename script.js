e8e8b8d25f153a4934b3eb6296581d46a5a41478
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2022-08-29 16:47:37 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const button = document.getElementById('button');
+const toasts = document.getElementById('toasts');
 
+const messageType = ['special', 'info', 'warning', 'error'];
+
+button.addEventListener('click', () => createNotification());
+
+function createNotification(message = null, type = null) {
+	const notif = document.createElement('div');
+	notif.classList.add('toast');
+
+	if (type === null) {
+		type = getRandom(messageType);
+	}
+
+	if (message === null) {
+		message = `Some ${type} message.`;
+	}
+
+	notif.classList.add(type);
+	notif.innerText = message;
+	// console.log(type, message);
+
+	toasts.appendChild(notif);
+
+	setTimeout(() => {
+		notif.remove();
+	}, 3000);
+}
+
+function getRandom(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+}
